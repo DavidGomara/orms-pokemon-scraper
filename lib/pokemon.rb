9ee10c0d23328f2f3adcc630ea71fbc216d87b1d
DavidGomara <davidgomara7@gmail.com>
2020-12-31 18:57:34 -0500

    commit tree 6c743cba47c50e46fae2f3ef8d2fed9aea8117ad

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(name, id=nil, type=nil, db=nil)
+        @name = name
+        @type = type
+        @id = id
+        @db = db
+    end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+
+        db.execute(sql,name,type)
+    end 
+
+    def self.find(x, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id 
+        IS ?;
+        SQL
+
+        pokemon_array = db.execute(sql,x).flatten
+        
+        Pokemon.new(pokemon_array[1], pokemon_array[0], pokemon_array[2], db)
+    end 
 end
